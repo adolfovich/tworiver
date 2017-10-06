412faf3f50fdc108cbc79dc1b8579498160f3833
Adolfovich <adolfovich.alexashka@gmail.com>
2017-10-06 14:53:15 +0300

    fix отображения новостей если дата истекла

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	}*/
 	
 	$curdate = date("Y-m-d");
-	$result_news = mysql_query("SELECT * FROM news WHERE date_end IS NULL OR date_end < $curdate ORDER BY important DESC, date_crate DESC") or die(mysql_error());
+	$result_news = mysql_query("SELECT * FROM news WHERE date_end IS NULL OR date_end >= $curdate ORDER BY important DESC, date_crate DESC") or die(mysql_error());
 ?>
 
 <!DOCTYPE html>
