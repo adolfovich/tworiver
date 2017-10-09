3061fbd779283d816183c9877985e1dca0b546e1
Adolfovich <adolfovich.alexashka@gmail.com>
2017-10-09 23:27:04 +0300

    в раздел новости добавлен вывод существующих новостей, форма добавления новости, форма редактирования новости

--- admin_news.php
+++ admin_news.php
@@ -17,111 +17,16 @@
 			$is_admin = $user_is_admin['is_admin'];
 		}
 		
-		//выбираем существующие тарифы
+		
 		
 		
 		
 		if ($is_admin == 1) {
 			
 			
-			if (isset($_GET['edit_user']) && strlen($_GET['edit_user']) != 0) {
-				$edit_user = $_GET['edit_user'];
-				//выбираем данные по пользователю 
-				$result_user_data = mysql_query("SELECT 
-														u.id, 
-														u.uchastok, 
-														u.email, 
-														u.name, 
-														u.phone, 
-														u.sch_model, 
-														u.sch_num, 
-														u.sch_plomb_num, 
-														u.balans, 
-														uc.num, 
-														uc.date_start 
-													FROM users u, users_contracts uc WHERE u.id = ".$_GET['edit_user']." AND u.id = uc.user AND uc.date_end IS NULL") or die(mysql_error());
-				//echo "SELECT u.id, u.uchastok, u.name, u.phone, u.sch_model, u.sch_num, u.sch_plomb_num, u.balans, uc.num, uc.date_start FROM users u, users_contracts uc WHERE u.id = ".$_GET['edit_user']." AND u.id = uc.user AND uc.date_end IS NULL";
-				
-				while ($user_data = mysql_fetch_assoc($result_user_data)) {
-					$user_id = $user_data['id'];
-					$user_email = $user_data['email'];
-					$user_uchastok = $user_data['uchastok'];
-					$user_name = $user_data['name'];
-					$user_phone = $user_data['phone'];
-					$user_sch_model = $user_data['sch_model'];
-					$user_sch_num = $user_data['sch_num'];
-					$user_sch_plomb_num = $user_data['sch_plomb_num'];
-					$user_contract_num = $user_data['num'];
-					$user_contract_date = $user_data['date_start'];
-				}
-			}
 			
+				
 			
-			if (isset($_GET['fio']) && strlen($_GET['fio'])!=0) {
-				$add_fio = $_GET['fio'];
-				$add_email = $_GET['email'];
-				$add_phone = $_GET['phone'];
-				$add_password = md5($_GET['password']);
-				$add_uchastok = $_GET['uchastok'];
-				$add_sch_model = $_GET['sch_model'];
-				$add_sch_num = $_GET['sch_num'];
-				$add_sch_pl_num = $_GET['sch_pl_num'];
-				$add_start_ind = $_GET['start_ind'];
-				$add_start_bal = $_GET['start_bal'];
-				$add_tarif1 = $_GET['tarif1'];
-				$add_tarif2 = $_GET['tarif2'];
-				$add_contract_num = $_GET['contract_num'];
-				$add_contract_date = $_GET['contract_date'];
-				
-				$q_upd_user = "UPDATE users SET 
-												name = '$add_fio', 
-												email = '$add_email', 
-												phone='$add_phone', 
-												uchastok = '$add_uchastok', 
-												sch_model = '$add_sch_model', 
-												sch_num = '$add_sch_num', 
-												sch_plomb_num = '$add_sch_pl_num' WHERE id = $edit_user";
-				//echo $q_upd_user;
-				mysql_query($q_upd_user) or die(mysql_error());
-				
-				
-				//Удаляем тарифы пользователя 
-				mysql_query("DELETE FROM users_tarifs WHERE user = $edit_user") or die(mysql_error());
-				
-				//Добавляем основной тариф
-				//echo 'Добавляем тариф1';
-				//echo '<br>';
-				$q_add_tarif1 = "INSERT INTO users_tarifs SET user = $edit_user, tarif = $add_tarif1";
-				//echo $q_add_tarif1;
-				//echo '<br>';
-				mysql_query($q_add_tarif1) or die(mysql_error());
-				
-				
-				if ($add_tarif2 != 0) {
-					//echo 'Добавляем тариф2';
-					//echo '<br>';
-					$q_add_tarif2 = "INSERT INTO users_tarifs SET user = $edit_user, tarif = $add_tarif2";
-					//echo $q_add_tarif2;
-					mysql_query($q_add_tarif2) or die(mysql_error());
-				}
-				
-				//Проверяем изменился ли договор
-				if ($user_contract_num != $add_contract_num) {
-					//закрываем предыдущий договор 
-					mysql_query("UPDATE users_contracts SET date_end = '$curdate' WHERE user = $edit_user") or die(mysql_error());
-					//Добавляем новый договор
-					mysql_query("INSERT INTO users_contracts SET user = $edit_user, type = 1, num = '$add_contract_num', date_start = '$add_contract_date'") or die(mysql_error());
-				}
-				
-				//Добавляем пользователю договор на энергопотребление
-				//$q_add_contract = "INSERT INTO users_contracts SET user = $add_user_id, type = 1, num = '$add_contract_num', date_start = '$add_contract_date'";
-				//mysql_query($q_add_contract) or die(mysql_error());
-				
-				echo '<script type="text/javascript">swal("", "Пользователь сохранен", "success")</script>';
-				
-				header("Location: admin_user_edit.php?edit_user=$edit_user");
-				
-			}
 			
 		}
 	}
@@ -152,6 +57,28 @@
 		<script src="js/sweetalert.min.js"></script>
 		<link rel="stylesheet" href="css/my.css">
 		
+		<script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
+ 
+		<script type="text/javascript">
+			tinymce.init({
+				selector: 'textarea',
+				language: 'ru',
+				height: 300,
+				theme: 'modern',
+				plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
+				toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+				image_advtab: true,
+				templates: [
+					{ title: 'Test template 1', content: 'Test 1' },
+					{ title: 'Test template 2', content: 'Test 2' }
+				],
+				content_css: [
+					'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+					'//www.tinymce.com/css/codepen.min.css'
+				]
+			});
+		</script>
+		
 
 		<style>
 			#header {
@@ -162,6 +89,12 @@
 			}
 			.news_date {
 				color: #777;
+			}
+			.del_user{
+				color:Crimson;
+			}
+			.del_user:hover{
+				color:red;
 			}
 		</style>
 		
@@ -192,16 +125,160 @@
 							</div>
 							<div class="row">
 								<div class="col-md-12">
-								  <h3>Редактирование новостей</h3>
-									<div class="table-responsive">
-									
-									
-											<
-									<br><br>
-									
-									</div>
+								  <h3>Управление новостями</h3>
+									<hr>
 								</div>
 								
+							</div>
+							<div class="row">
+								<a href="#addNews" class="btn btn-primary" data-toggle="modal"><i class="fa fa-plus" aria-hidden="true"></i> Добавить Новость</a>
+								<!-- HTML-код модального окна -->
+									<div id="addNews" class="modal fade">
+									  <div class="modal-dialog">
+										<div class="modal-content">
+										  <!-- Заголовок модального окна -->
+										  <div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+											<h4 class="modal-title">Добавление новости</h4>
+										  </div>
+										  <!-- Основное содержимое модального окна -->
+										  <div class="modal-body">
+											<form enctype="multipart/form-data" method="POST" id="addContactForm">
+												<div class="form-group">
+													<label for="theme">Тема</label>
+													<input type="text" name="theme" class="form-control">
+												</div>
+												<div class="form-group">
+													<label for="dateEnd">Дата окончания публикации</label>
+													<input type="date" name="dateEnd" class="form-control">
+												</div>
+												<div class="form-group">
+													<div class="col-sm-offset-2 col-sm-10">
+													  <div class="checkbox">
+														<label>
+														  <input name="important" type="checkbox"> Важная
+														</label>
+													  </div>
+													</div>
+												  </div>
+																								
+												
+												<div class="form-group">
+													<label for="text">Текст</label>
+													<textarea name="text" class="form-control" rows="3"></textarea>
+													
+													
+												</div>																				
+												
+											</form>
+										  </div>
+										  <!-- Футер модального окна -->
+										  <div class="modal-footer">
+											<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+											<button type="button" class="btn btn-primary" onclick="document.getElementById('addContactForm').submit(); return false;" >Сохранить</button>
+										  </div>
+										</div>
+									  </div>
+									</div>
+								<br>
+								<?php $result_news = mysql_query("SELECT * FROM news WHERE is_del = 0") or die(mysql_error()); ?>
+								<div class="col-md-12" style="margin-top: 20px;">
+									<h4>Существующие новости</h3>
+									<table class="table table-condensed">
+										<tr>
+											<th>Дата публикации</th>
+											<th>Тема</th>
+											<th>Текст</th>
+											<th>Дата окончания публикации</th>
+											<th>Важная</th>
+											<th></th>
+											<th></th>
+										</tr>
+										<?php
+										while ($news = mysql_fetch_assoc($result_news)) {
+											echo '<tr>';
+											echo '<td>'.date( 'd.m.Y',strtotime($news['date_crate'])).'</td>';
+											echo '<td>'.$news['header'].'</td>';
+											$words = explode(' ',$news['text']);
+											if(count($words) > 20 && 20 > 0) {
+												$text = implode(' ',array_slice($words, 0, 20)).'...';
+											}
+											echo '<td>'.$text.'</td>';
+											if (is_null($news['date_end'])) {
+												echo '<td>Не устоновлена</td>';
+												$date_end = '';
+												
+											}
+											else {
+												echo '<td>'.date( 'd.m.Y',strtotime($news['date_end'])).'</td>';
+												$date_end = date( 'Y-m-d',strtotime($news['date_end']));
+											}
+											
+											if ($news['important'] == 1) {
+												echo '<td style="text-align: center;"><input type="checkbox" checked disabled></td>';
+											}
+											else {
+												echo '<td style="text-align: center;"><input type="checkbox" disabled></td>';
+											}
+																						
+											echo '<td><a href="#editNews'.$news['id'].'" data-toggle="modal"><i class="fa fa-pencil" aria-hidden="true" title="Редактировать новость"></i></a>
+											<!-- HTML-код модального окна -->
+												<div id="editNews'.$news['id'].'" class="modal fade">
+												  <div class="modal-dialog">
+													<div class="modal-content">
+													  <!-- Заголовок модального окна -->
+													  <div class="modal-header">
+														<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+														<h4 class="modal-title">Редактирование новости</h4>
+													  </div>
+													  <!-- Основное содержимое модального окна -->
+													  <div class="modal-body">
+														<form method="POST" role="form" id="formEditNews'.$news['id'].'">
+															<input name="editedNews" type="hidden" value="'.$news['id'].'">
+															<div class="form-group">
+																<label for="theme">Тема</label>
+																<input type="text" name="theme" class="form-control" value="'.$news['header'].'">
+															</div>
+															<div class="form-group">
+																<label for="dateEnd">Дата окончания публикации</label>
+																<input type="date" name="dateEnd" class="form-control" value="'.$date_end.'">
+															</div>
+															<div class="form-group">
+																<div class="col-sm-offset-2 col-sm-10">
+																  <div class="checkbox">
+																	<label>';
+																	if ($news['important'] == 1) {
+																		echo '<input name="important" type="checkbox" checked> Важная';
+																	}
+																	else {
+																		echo '<input name="important" type="checkbox"> Важная';
+																	}
+																	  
+											echo '					</label>
+															  </div>
+																</div>
+															  </div>
+															<div class="form-group">
+																<label for="text">Текст</label>
+																<textarea name="text" class="form-control" rows="3">'.$news['text'].'</textarea>
+															</div>	
+														</form>
+													  </div>
+													  <!-- Футер модального окна -->
+													  <div class="modal-footer">
+														<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+														<button type="button" class="btn btn-primary" onclick="document.getElementById(\'formEditNews'.$news['id'].'\').submit(); return false;" >Сохранить</button>
+													  </div>
+													</div>
+												  </div>
+												</div>
+											</td>';
+											echo '<td><a class="del_user" href="#" onclick="ConfirmDelNews('.$news['id'].')"><i class="fa fa-trash" aria-hidden="true" title="Удалить новость"></i></a></td>';
+											echo '</tr>';
+										}
+										?>
+									</table>
+								</div>
 							</div>
 						
 						<?php 
