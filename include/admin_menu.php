05257a34c10007d7889632eb7301f65f05e55807
Adolfovich <adolfovich.alexashka@gmail.com>
2017-10-30 10:36:16 +0300

    В админ панели показания и платежи для удобства объединены во вкладку Электроэнергия

--- include/admin_menu.php
+++ include/admin_menu.php
@@ -1,8 +1,8 @@
 <nav>
 	<ul class="nav navbar-nav">
 		<li><a href="admin_users.php">Пользователи</a></li>
-		<li><a href="admin_indications.php">Показания</a></li>
-		<li><a href="admin_payments.php">Платежи</a></li>
+		<li><a href="admin_electric.php">Электроэнергия</a></li>
+		
 		<li><a href="admin_contribution.php">Взносы</a></li>
 		<li><a href="admin_reports_fhd.php">отчеты ФХД</a></li>
 		<li><a href="admin_news.php">Новости</a></li>
