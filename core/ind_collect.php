f66b3dbd04adde3e1660682107b9fa11b3da7c13
Adolfovich <adolfovich.alexashka@gmail.com>
2017-12-01 19:45:32 +0300

    fix ind_collect

--- core/ind_collect.php
+++ core/ind_collect.php
@@ -6,8 +6,9 @@
 	$result_users = mysql_query("SELECT * FROM users WHERE modem_num IS NOT NULL") or die(mysql_error());
 	
 	while ($users = mysql_fetch_assoc($result_users)) {
-		
-		$indications = $obj = json_decode(file_get_contents('https://lk.waviot.ru/api/report/?template=a2f5261236bf3e2aede89cae168d2c2d&period=P1D&from='.$curdate.'&raw=1&modem='.$users['modem_num'].'&to='.$curdate));
+		$indication_q = 'https://lk.waviot.ru/api/report/?template=a2f5261236bf3e2aede89cae168d2c2d&period=P1D&from='.$curdate.'&raw=1&modem='.$users['modem_num'].'&to='.$curdate;
+		//echo $indication_q;
+		$indications = $obj = json_decode(file_get_contents($indication_q));
 		
 		$count_tarifs = count((array)$indications);
 		//var_dump($indications);
@@ -34,7 +35,7 @@
 				}
 				$ind_diff = $value - $prev_ind;
 				//добавляем показания
-				$insert_q = "INSERT INTO Indications SET date='".$curdate."', user=".$users['id'].", tarif=(SELECT id FROM tarifs WHERE id_waviot = '".$tarif."'), Indications=".$value.", prev_indications = '".$prev_ind."', additional=(SELECT price FROM tarifs WHERE id_waviot = '".$tarif."'), additional_sum= $ind_diff*(SELECT price FROM tarifs WHERE id_waviot = '".$tarif."'), auto = 1";
+				$insert_q = "INSERT INTO Indications SET date='".date("Y-m-d",strtotime($curdate)-86400)."', user=".$users['id'].", tarif=(SELECT id FROM tarifs WHERE id_waviot = '".$tarif."'), Indications=".$value.", prev_indications = '".$prev_ind."', additional=(SELECT price FROM tarifs WHERE id_waviot = '".$tarif."'), additional_sum= $ind_diff*(SELECT price FROM tarifs WHERE id_waviot = '".$tarif."'), auto = 1";
 				//echo $insert_q;
 				mysql_query($insert_q) or die(mysql_error());
 				//Обновляем баланс пользователя
