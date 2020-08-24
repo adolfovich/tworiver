2197ea5454d43095c5f5ac70db6c1595d8ceb9d7
root <root>
2020-08-24 16:08:09 +0300

    fix sorting in admin_users

--- pages/ajax/searchUsers.php
+++ pages/ajax/searchUsers.php
@@ -10,23 +10,46 @@ $core  = new Core();
 
 $form = $core->form;
 
-if ($form['sorting'] == 'area') {
-  $ordering = ' ORDER BY uchastok';
-} else if ($form['sorting'] == 'name') {
-  $ordering = ' ORDER BY name';
-} else if ($form['sorting'] == 'balance') {
-  $ordering = ' ORDER BY balance';
-} else {
-  $ordering = '';
-}
+
+
+$ordering = '';
+
+
 
 $sql = $db->parse("SELECT u.*, (SELECT SUM(balance) FROM purses WHERE user_id = u.id) as balance FROM users u WHERE u.is_del = 0 AND (u.name LIKE ?s OR u.email LIKE ?s OR u.phone LIKE ?s OR u.uchastok LIKE ?s)".$ordering, '%'.$form['string'].'%', '%'.$form['string'].'%', '%'.$form['string'].'%', '%'.$form['string'].'%');
 
 $users = $db->getAll($sql);
 
+
+if ($form['sorting'] == 'area') {
+  foreach ($users as $key => $value) {
+    $users_arr[$value['id']] = $value['uchastok'];
+  }
+  natsort($users_arr);
+} else if ($form['sorting'] == 'name') {
+  foreach ($users as $key => $value) {
+    $users_arr[$value['id']] = $value['name'];
+  }
+  natsort($users_arr);
+} else if ($form['sorting'] == 'balance') {
+  foreach ($users as $key => $value) {
+    $users_arr[$value['id']] = $value['balance'];
+  }
+  asort($users_arr);
+} else {
+  foreach ($users as $key => $value) {
+    $users_arr[$value['id']] = $value['id'];
+  }
+  asort($users_arr);
+}
+
+
 $html = '';
 
-foreach ($users as $user) {
+foreach ($users_arr as $user_id => $user_area) {
+  $user = $db->getRow("SELECT * FROM users WHERE id = ?i", $user_id);
+  $user_balances = $db->getOne("SELECT SUM(balance) FROM purses WHERE user_id = ?i", $user['id']);
+
   $html .= '<tr>';
   $html .= '<td scope="row">';
   $html .= '<div class="media align-items-center">';
@@ -60,7 +83,7 @@ foreach ($users as $user) {
   }
   $html .= '</td>';
   //$u_balance = $db->getOne("SELECT SUM(balance) FROM purses WHERE user_id = ?i", $user['id']);
-  $html .= '<td class="budget">'.$user['balance'].'р.</td>';
+  $html .= '<td class="budget">'.$user_balances.'р.</td>';
   $html .= '<td class="budget">';
   $html .= '<a class="text-danger" href="?action=delete&id='.$user['id'].'"><i class="fa fa-ban" aria-hidden="true"></i></a>';
   $html .= '</td>';
